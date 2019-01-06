b8a310259468cf792b8ee662c43cb69abdf5e06b
凌志伟 <lingzw@cn.wilmar-intl.com>
2019-01-07 00:01:03 +0800

    Add custom pages.

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""定义 learning_logs 的 URL 模式"""
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+  # 主页
+  path('', views.index, name='index'),
+  path('topics/', views.topics, name='topics'),
+  path('topic/<int:topic_id>', views.topic, name='topic')
+]
